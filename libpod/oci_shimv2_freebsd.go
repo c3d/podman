cbc059f179cb3cdeb9ccd50f18119c9881f90800
Christophe de Dinechin <dinechin@redhat.com>
2024-01-18 11:54:16 +0100

    Split Conmon and ShimV2 interfaces, factor out shared code
    Some additional code splitting / duplication to separate ShimV2 and
    Conmon interfaces.
    
    - Code that is shared is moved to `oci_common.go`
    - Move diverging code to `oci_shimv2_{common,linux,freebsd}.go`
    
    Fixes: #7

---
+++ libpod/oci_shimv2_freebsd.go
@@ -0,0 +1,33 @@
+//go:build !remote
+
+package libpod
+
+import (
+	"errors"
+	"os"
+	"os/exec"
+)
+
+func (r *ShimV2OCIRuntime) createRootlessContainer(ctr *Container, restoreOptions *ContainerCheckpointOptions) (int64, error) {
+	return -1, errors.New("unsupported (*ShimV2OCIRuntime) createRootlessContainer")
+}
+
+// Run the closure with the container's socket label set
+func (r *ShimV2OCIRuntime) withContainerSocketLabel(ctr *Container, closure func() error) error {
+	// No label support yet
+	return closure()
+}
+
+// moveShimV2ToCgroupAndSignal gets a container's cgroupParent and moves the shimV2 process to that cgroup
+// it then signals for shimV2 to start by sending nonce data down the start fd
+func (r *ShimV2OCIRuntime) moveShimV2ToCgroupAndSignal(ctr *Container, cmd *exec.Cmd, startFd *os.File) error {
+	// No equivalent to cgroup on FreeBSD, just signal shimV2 to start
+	if err := writeShimV2PipeData(startFd); err != nil {
+		return err
+	}
+	return nil
+}
+
+func moveToRuntimeCgroup() error {
+	return errors.New("moveToRuntimeCgroup not supported on freebsd")
+}
